6a897912d8034d8c521708d16022dcb7564081c0
dapi <danil@orionet.ru>
2013-12-18 13:23:32 +0400

    "Automated commit at 2013-12-18 09:23:32 UTC by middleman-deploy 0.1.4"

--- javascripts/urls.js
+++ javascripts/urls.js
@@ -1,1 +1,1 @@
-!function(){this.App.goto=function(e){return window.location.href=e},this.App.goto_key=function(){return this.App.goto(Eludia.Helpers.ApplicationHelpers.url(url_name))}}.call(this);
+!function(){this.App.goto=function(n){return window.location.href=n},this.App.goto_key=function(){return this.App.goto(Eludia.Helpers.ApplicationHelpers.url(url_name))}}.call(this);
